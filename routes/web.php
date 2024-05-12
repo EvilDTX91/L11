390c8e0b26c08c6b79960d19fb90fa968b1c0053
Nádas Tamás <nadas.tamas@symboltech.hu>
2024-05-13 00:50:46 +0200

    Add RickAndMorty routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\RickAndMorty\RickAndMortyController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['web'])->group(function () {
+    Route::get('/rickandmorty', [RickAndMortyController::class, 'List'])->name('List');
+    Route::get('/rickandmorty/init', [RickAndMortyController::class, 'InitDataURL'])->name('InitDataURL');
+    Route::post('/rickandmorty/init', [RickAndMortyController::class, 'AjInitData'])->name('AjInitData');
+    Route::post('/rickandmorty/database/clear', [RickAndMortyController::class, 'AjClearDatabase'])->name('AjClearDatabase');
+    Route::post('/rickandmorty/episode/characters', [RickAndMortyController::class, 'AjGetEpisodeCharacters'])->name('AjGetEpisodeCharacters');
 });
